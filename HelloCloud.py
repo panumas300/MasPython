30e543dd8b95e861929f256fb53252ad378407f4
Panumas <panumas300@outlook.com>
2021-08-11 11:13:42 +0700

    Modify Title

--- HelloCloud.py
+++ HelloCloud.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Panumas RUK-COM!</h1>'
+    return '<h1>Hello Panumas RUK-COM is Great!</h1>'
 
 
 @app.route('/home',methods=['GET','POST'])
