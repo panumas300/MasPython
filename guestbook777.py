f51b6fdbd7c8bbc37e0b773c5e316b3c879a22f8
Panumas <panumas300@outlook.com>
2021-08-11 12:30:01 +0700

    gb test 2

--- guestbook777.py
+++ guestbook777.py
@@ -19,8 +19,8 @@ class Comments(db.Model):
 @app.route('/')
 def index():
 #    result = Comments.query.all()
-#    return render_template('index7.html', result=result)
-    return 'Hello World'
+    return render_template('index7.html', result=result)
+
 
 @app.route('/sign')
 def sign():
